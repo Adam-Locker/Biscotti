3587d6611627cf98c6966cbf8cb0ad4c9b2a75fa
Adam-Locker <adam.locker@ht-la.org>
2015-10-21 17:27:25 -0700

    fixed errors in Robot

--- src/org/usfirst/frc/team4/robot/Robot.java
+++ src/org/usfirst/frc/team4/robot/Robot.java
@@ -26,11 +26,6 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-<<<<<<< HEAD
-    
-    //Hi
-=======
->>>>>>> 2f80b6e3ebbbf26803f628a3699a5494ea687383
     public void robotInit() {
 		oi = new OI();
         // instantiate the command used for the autonomous period
